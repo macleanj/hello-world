a6c4d2c2d22dbfc0417a3ed48625cedee34143a8
Jerome Mac Lean <jerome@crosslogic-consulting.com>
2020-01-29 10:05:17 +0100

    update all branch and deploy environment values to be able to continue testing.

--- build/config/_confConvert.sh
+++ build/config/_confConvert.sh
@@ -5,10 +5,11 @@ now=$(date +%Y-%m-%d\ %H:%M:%S)
 programName=$(basename $0)
 programDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 baseName=$(echo ${programName} | sed -e 's/.sh//g')
-debug=1
-unnamedFile=develop
+debug=0
+gitBranchNameDev=$(grep GIT_BRANCH_NAME_DEVELOP generic.conf | sed -e 's/.*=//g' | sed -e 's/[#].*//g' | sed -e 's/"//g' | awk '{$1=$1};1')
 
 cd $programDir
+rm -rf env.files
 mkdir -p env.files
 for file in $(ls *.conf); do
   [ $debug -eq 1 ] && echo "Processing $file"
@@ -17,22 +18,25 @@ for file in $(ls *.conf); do
   baseNameFile=$(echo ${file} | sed -e 's/.conf//g')
   
   # Prepare file(s) for environment variable format
-  [[ $baseNameFile =~ _$ ]] && baseNameFile="${baseNameFile}${unnamedFile}"
+  [[ $baseNameFile =~ _$ ]] && baseNameFile="${baseNameFile}${gitBranchNameDev}"
   echo -n > env.files/${baseNameFile}.env
 
   # Prepare file(s) for groovy format
   echo -n > env.files/${baseNameFile}.groovy
 
-  # Convert to envrionment variable format
+  # Convert
   OLDIFS=$IFS
   IFS=$'\n'
   for env in $(cat $file | egrep -v "^#|^[[:space:]]"); do
-    echo $env
+    [ $debug -eq 1 ] && echo $env
     key=$(echo $env | sed -e 's/=.*//g' | awk '{$1=$1};1')
-    value=$(echo $env | sed -e 's/.*=//g' | awk '{$1=$1};1')
+    value=$(echo $env | sed -e 's/.*=//g' | sed -e 's/[#].*//g' | awk '{$1=$1};1')
     [[ ! $value =~ ^\" ]] && value="\"$value\""
 
+    # Environment variable format
     echo "$key=$value" >> env.files/${baseNameFile}.env
+
+    # Environment groovy format
     echo "env.$key=$value" >> env.files/${baseNameFile}.groovy
 
   done
