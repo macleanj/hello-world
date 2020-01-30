2bcfcd8860c93d97c54a6428703dcf6c5862e4ff
Jerome Mac Lean <jerome@crosslogic-consulting.com>
2020-01-30 22:58:04 +0100

    Rework of tag decisions

--- build/config/_prepTagEnvConvert.sh
+++ build/config/_prepTagEnvConvert.sh
@@ -11,9 +11,10 @@ debug=0
 
 source $programDir/generic.conf
 CICD_TAGS_NAME=$1
+[ -z $GIT_COMMIT_SHORT ] && imageHash="DummyHash1345fa4f3546a3" || imageHash=$GIT_COMMIT_SHORT
 
-buildTagStartingWith=$CICD_TAGS_BUILD_TAG
-deployTagStartingWith=$CICD_TAGS_DEPLOY_TAG
+buildTagType=$CICD_TAGS_BUILD_TAG
+deployTagType=$CICD_TAGS_DEPLOY_TAG
 
 declare -A mapTag2imageType
 for mapping in $CICD_TAGS_TAG_MAPPING; do
@@ -31,25 +32,25 @@ else
   buildEnabled=1
   currentTag=$CICD_TAGS_NAME
   # Character extraction by expected format
-  # - Build: $buildTagStartingWith<branchCharacter>-<version>. Example: vm-1.01
-  # - Deploy: $deployTagStartingWith<branchCharacter>-<deployEnvironment>-<version>. Example: ds-1.01
+  # - Build: $buildTagType<imageTypeKey>-<version>. Example: bh-1.01
+  # - Deploy: $deployTagType<imageTypeKey>-<deployEnvironment>-<version>. Example: dv-prod-1.01
   tagTypeKey="${currentTag:0:1}"
   imageTypeKey="${currentTag:1:1}"
-  if [[ "$tagTypeKey" == "${buildTagStartingWith}" ]]; then
+  if [[ "$tagTypeKey" == "${buildTagType}" ]]; then
     envKey="NA"
-    trackingNumber=$(echo $currentTag | awk -F "-" '{print $2}')
-  elif [[ "$tagTypeKey" == "${deployTagStartingWith}" ]]; then
+    versionKey=$(echo $currentTag | awk -F "-" '{print $2}')
+  elif [[ "$tagTypeKey" == "${deployTagType}" ]]; then
     envKey=$(echo $currentTag | awk -F "-" '{print $2}')
-    trackingNumber=$(echo $currentTag | awk -F "-" '{print $3}')
+    versionKey=$(echo $currentTag | awk -F "-" '{print $3}')
   fi
   [ $debug -eq 1 ] && echo "Receiving tag: $currentTag"
   [ $debug -eq 1 ] && echo "tagTypeKey: $tagTypeKey"
   [ $debug -eq 1 ] && echo "imageTypeKey: $imageTypeKey"
   [ $debug -eq 1 ] && echo "envKey: $envKey"
-  [ $debug -eq 1 ] && echo "trackingNumber: $trackingNumber"
+  [ $debug -eq 1 ] && echo "versionKey: $versionKey"
 
   # tagTypeKey character
-  [[ "$tagTypeKey" != "${buildTagStartingWith}" && "$tagTypeKey" != "${deployTagStartingWith}" ]] && buildEnabled=0
+  [[ "$tagTypeKey" != "${buildTagType}" && "$tagTypeKey" != "${deployTagType}" ]] && buildEnabled=0
   [ $debug -eq 1 ] && echo -e "1: Enabled: $buildEnabled\n"
 
   # imageTypeKey character
@@ -59,19 +60,21 @@ else
 
   # Trailing characters
   if [[ $buildEnabled == 1 ]]; then
-    if [[ "$tagTypeKey" == "${buildTagStartingWith}" ]]; then
+    if [[ "$tagTypeKey" == "${buildTagType}" ]]; then
       # Build tag received
       CICD_TAGS_BUILD_IMAGE_TYPE=${mapTag2imageType[$imageTypeKey]}
-      CICD_TAGS_BUILD_VERSION=${trackingNumber}
+      CICD_TAGS_BUILD_VERSION=${versionKey}
+      [[ ${mapTag2imageType[$imageTypeKey]} == "hash" ]] && CICD_TAGS_BUILD_VERSION=$imageHash
 
       # Build tag format
       [[ ! $currentTag =~ ^[a-z]+-[0-9.]+$ ]] && buildEnabled=0
       [ $debug -eq 1 ] && echo -e "3: Enabled: $buildEnabled"
-    elif [[ "$tagTypeKey" == "${deployTagStartingWith}" ]]; then
+    elif [[ "$tagTypeKey" == "${deployTagType}" ]]; then
       # Deploy tag received
       CICD_TAGS_DEPLOY_IMAGE_TYPE=${mapTag2imageType[$imageTypeKey]}
       CICD_TAGS_DEPLOY_ENVIRONMENT=${envKey}
-      CICD_TAGS_DEPLOY_RELEASE=${trackingNumber}
+      CICD_TAGS_DEPLOY_VERSION=${versionKey}
+      [[ ${mapTag2imageType[$imageTypeKey]} == "hash" ]] && CICD_TAGS_DEPLOY_VERSION=$imageHash
 
       # Deploy tag format
       [[ ! $currentTag =~ ^[a-z]+-[a-z]+-[0-9.]+$ ]] && buildEnabled=0
@@ -83,38 +86,36 @@ else
     fi
     # No else needed. Code is protected earlier
   fi
-
-  # [[ ! $currentTag =~ ^[a-z]${buildTagStartingWith} && ! $currentTag =~ ^${deployTagStartingWith} ]] && buildEnabled=0
   [ $debug -eq 1 ] && echo -e "Enabled: $buildEnabled\n"
 fi
 [ $debug -eq 1 ] && echo "------------------------------------------------------------------------------------------"
 [ $debug -eq 1 ] && echo "CICD_TAGS_BUILD_IMAGE_TYPE: $CICD_TAGS_BUILD_IMAGE_TYPE"
-[ $debug -eq 1 ] && echo "CICD_TAGS_BUILD_ENVIRONMENT: NA"
+[ $debug -eq 1 ] && echo "CICD_TAGS_BUILD_ENVIRONMENT: $CICD_TAGS_BUILD_ENV"
 [ $debug -eq 1 ] && echo "CICD_TAGS_BUILD_VERSION: $CICD_TAGS_BUILD_VERSION"
 [ $debug -eq 1 ] && echo "------------------------------------------------------------------------------------------"
 [ $debug -eq 1 ] && echo "CICD_TAGS_DEPLOY_IMAGE_TYPE: $CICD_TAGS_DEPLOY_IMAGE_TYPE"
 [ $debug -eq 1 ] && echo "CICD_TAGS_DEPLOY_ENVIRONMENT: $CICD_TAGS_DEPLOY_ENVIRONMENT"
-[ $debug -eq 1 ] && echo "CICD_TAGS_DEPLOY_RELEASE: $CICD_TAGS_DEPLOY_RELEASE"
+[ $debug -eq 1 ] && echo "CICD_TAGS_DEPLOY_VERSION: $CICD_TAGS_DEPLOY_VERSION"
 [ $debug -eq 1 ] && echo "------------------------------------------------------------------------------------------"
 [ $debug -eq 1 ] && echo "Build enabled: $buildEnabled"
 
 echo "CICD_BUILD_ENABLED=\"$buildEnabled\"" > ${tagFile}
 if [[ $buildEnabled == 1 ]]; then
-  if [[ "$tagTypeKey" == "${buildTagStartingWith}" ]]; then
+  if [[ "$tagTypeKey" == "${buildTagType}" ]]; then
     # Build tag received
 cat >> ${tagFile} <<EOL
 CICD_TAGS_TAG_TYPE="Build"
 CICD_TAGS_IMAGE_TYPE="$CICD_TAGS_BUILD_IMAGE_TYPE"
-CICD_TAGS_DEPLOY_ENVIRONMENT="None"
+CICD_TAGS_DEPLOY_ENVIRONMENT="$CICD_TAGS_BUILD_ENV"
 CICD_TAGS_ID="$CICD_TAGS_BUILD_VERSION"
 EOL
-  elif [[ "$tagTypeKey" == "${deployTagStartingWith}" ]]; then
+  elif [[ "$tagTypeKey" == "${deployTagType}" ]]; then
     # Deploy tag received
 cat >> ${tagFile} <<EOL
 CICD_TAGS_TAG_TYPE="Deployment"
 CICD_TAGS_IMAGE_TYPE="$CICD_TAGS_DEPLOY_IMAGE_TYPE"
 CICD_TAGS_DEPLOY_ENVIRONMENT="$CICD_TAGS_DEPLOY_ENVIRONMENT"
-CICD_TAGS_ID="$CICD_TAGS_DEPLOY_RELEASE"
+CICD_TAGS_ID="$CICD_TAGS_DEPLOY_VERSION"
 EOL
   else
 cat >> ${tagFile} <<EOL
