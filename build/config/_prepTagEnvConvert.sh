3f69242933b17a0de9fc865f2711e2e5689aef13
Jerome Mac Lean <jerome@crosslogic-consulting.com>
2020-01-30 21:55:42 +0100

    Rework of tag decisions

--- build/config/_prepTagEnvConvert.sh
+++ build/config/_prepTagEnvConvert.sh
@@ -16,20 +16,20 @@ buildTagStartingWith=$CICD_TAGS_BUILD_TAG
 deployTagStartingWith=$CICD_TAGS_DEPLOY_TAG
 
 declare -A mapTag2Branch
-for mapping in $CICD_TAGS_BRANCH_MAPPING; do
+for mapping in $CICD_TAGS_TAG_MAPPING; do
   key=$(echo $mapping | awk -F "=" '{print $1}')
   value=$(echo $mapping | awk -F "=" '{print $2}')
   mapTag2Branch[$key]=$value
 done
 
 
-declare -A mapTag2Environment
-# CICD_TAGS_DEPLOY_ENV_MAPPING="dev=sandbox test=f-prod stag=int prod=prod"
-for mapping in $CICD_TAGS_DEPLOY_ENV_MAPPING; do
-  key=$(echo $mapping | awk -F "=" '{print $1}')
-  value=$(echo $mapping | awk -F "=" '{print $2}')
-  mapTag2Environment[$key]=$value
-done
+# declare -A mapTag2Environment
+# # CICD_TAGS_DEPLOY_ENV_LIST="dev=sandbox test=f-prod stag=int prod=prod"
+# for mapping in $CICD_TAGS_DEPLOY_ENV_LIST; do
+#   key=$(echo $mapping | awk -F "=" '{print $1}')
+#   value=$(echo $mapping | awk -F "=" '{print $2}')
+#   mapTag2Environment[$key]=$value
+# done
 
 # Verify typeKey tag character
 if [[ "$CICD_TAGS_NAME" == "" ]] && [[ "$CICD_TAGS_NAME" == "None" ]]; then
@@ -59,31 +59,34 @@ else
 
   # typeKey character
   [[ "$typeKey" != "${buildTagStartingWith}" && "$typeKey" != "${deployTagStartingWith}" ]] && buildEnabled=0
+  [ $debug -eq 1 ] && echo -e "1: Enabled: $buildEnabled\n"
 
   # branchKey character
   tmpKey=" $branchKey "
   [[ ! " ${!mapTag2Branch[@]} " =~ \s*$tmpKey\s* ]] && buildEnabled=0
+  [ $debug -eq 1 ] && echo -e "2: Enabled: $buildEnabled"
 
   # Trailing characters
   if [[ $buildEnabled == 1 ]]; then
     if [[ "$typeKey" == "${buildTagStartingWith}" ]]; then
       # Build tag received
-      CICD_TAGS_BUILD_BRANCH=${mapTag2Branch[$branchKey]}
       CICD_TAGS_BUILD_VERSION=${trackingNumber}
 
       # Build tag format
-      [[ ! $currentTag =~ [a-z]+-[0-9.]+ ]] && buildEnabled=0
+      [[ ! $currentTag =~ ^[a-z]+-[0-9.]+$ ]] && buildEnabled=0
+      [ $debug -eq 1 ] && echo -e "3: Enabled: $buildEnabled"
     elif [[ "$typeKey" == "${deployTagStartingWith}" ]]; then
       # Deploy tag received
-      CICD_TAGS_DEPLOY_BRANCH=${mapTag2Branch[$branchKey]}
-      CICD_TAGS_DEPLOY_ENVIRONMENT=${mapTag2Environment[${envKey}]}
+      CICD_TAGS_DEPLOY_ENVIRONMENT=${envKey}
       CICD_TAGS_DEPLOY_RELEASE=${trackingNumber}
 
       # Deploy tag format
-      [[ ! $currentTag =~ [a-z]+-[a-z]+-[0-9.]+ ]] && buildEnabled=0
+      [[ ! $currentTag =~ ^[a-z]+-[a-z]+-[0-9.]+$ ]] && buildEnabled=0
+      [ $debug -eq 1 ] && echo -e "3: Enabled: $buildEnabled"
       # envKey string
       tmpKey=" $envKey "
-      [[ ! " ${!mapTag2Environment[@]} " =~ $tmpKey ]] && buildEnabled=0
+      [[ ! " ${CICD_TAGS_DEPLOY_ENV_LIST} " =~ $tmpKey ]] && buildEnabled=0
+      [ $debug -eq 1 ] && echo -e "4: Enabled: $buildEnabled"
     fi
     # No else needed. Code is protected earlier
   fi
@@ -92,10 +95,9 @@ else
   [ $debug -eq 1 ] && echo -e "Enabled: $buildEnabled\n"
 fi
 [ $debug -eq 1 ] && echo "------------------------------------------------------------------------------------------"
-[ $debug -eq 1 ] && echo "CICD_TAGS_BUILD_BRANCH: $CICD_TAGS_BUILD_BRANCH"
+[ $debug -eq 1 ] && echo "CICD_TAGS_BUILD_ENVIRONMENT: NA"
 [ $debug -eq 1 ] && echo "CICD_TAGS_BUILD_VERSION: $CICD_TAGS_BUILD_VERSION"
 [ $debug -eq 1 ] && echo "------------------------------------------------------------------------------------------"
-[ $debug -eq 1 ] && echo "CICD_TAGS_DEPLOY_BRANCH: $CICD_TAGS_DEPLOY_BRANCH"
 [ $debug -eq 1 ] && echo "CICD_TAGS_DEPLOY_ENVIRONMENT: $CICD_TAGS_DEPLOY_ENVIRONMENT"
 [ $debug -eq 1 ] && echo "CICD_TAGS_DEPLOY_RELEASE: $CICD_TAGS_DEPLOY_RELEASE"
 [ $debug -eq 1 ] && echo "------------------------------------------------------------------------------------------"
@@ -107,7 +109,6 @@ if [[ $buildEnabled == 1 ]]; then
     # Build tag received
 cat >> ${tagFile} <<EOL
 CICD_TAGS_TYPE="Build"
-CICD_TAGS_BRANCH="$CICD_TAGS_BUILD_BRANCH"
 CICD_TAGS_DEPLOY_ENVIRONMENT="None"
 CICD_TAGS_ID="$CICD_TAGS_BUILD_VERSION"
 EOL
@@ -115,14 +116,12 @@ EOL
     # Deploy tag received
 cat >> ${tagFile} <<EOL
 CICD_TAGS_TYPE="Deployment"
-CICD_TAGS_BRANCH="$CICD_TAGS_DEPLOY_BRANCH"
 CICD_TAGS_DEPLOY_ENVIRONMENT="$CICD_TAGS_DEPLOY_ENVIRONMENT"
 CICD_TAGS_ID="$CICD_TAGS_DEPLOY_RELEASE"
 EOL
   else
 cat >> ${tagFile} <<EOL
-CICD_TAGS_TYPE="Branch"
-CICD_TAGS_BRANCH="None"
+CICD_TAGS_TYPE="Other"
 CICD_TAGS_DEPLOY_ENVIRONMENT="None"
 CICD_TAGS_ID="None"
 EOL
