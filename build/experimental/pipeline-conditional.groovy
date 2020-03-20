7b8450fa7b527eded9c42cdde4a4e41668a18117
Jerome Mac Lean <jerome@mac-lean.com>
2020-03-20 16:33:43 +0100

    cleanup

--- build/experimental/pipeline-conditional.groovy
+++
@@ -1,6 +0,0 @@
-// jenkins-library needs to be loaded as cicd
-@Library('cicd@develop') _cicd
-// If this job should also run when the library got changes, set changelog=true
-// @Library( value = "cicd@master", changelog = false ) _cicd
-
-_testConditionalPipline(currentBuild.getNumber())
